01ba82396d9eb93b7e3190ef0a6f20aba6f84e4d
mynameisjc <47384221+mynameisjc@users.noreply.github.com>
2019-10-17 23:01:20 -0700

    Add files via upload

--- SQL_exam.sql
+++ SQL_exam.sql
@@ -1,28 +1,30 @@
 --1. Inspect the naep table
-SELECT *
-FROM naep;
+SELECT table_name
+FROM information_schema.tables
+WHERE TABLE_NAME = 'naep' OR TABLE_NAME = 'finance';
+
+SELECT table_name, column_name
+FROM information_schema.columns
+WHERE TABLE_NAME = 'naep' OR TABLE_NAME = 'finance';
 
 --2. Limit to 50 results
 SELECT *
 FROM naep
 LIMIT 50;
 
---3. Summarizing data with averages
-SELECT state, AVG(avg_math_4_score)
+--3. Summarizing data 
+SELECT state, ROUND(AVG(avg_math_4_score),2), MIN(avg_math_4_score),
+	MAX(avg_math_4_score), COUNT(avg_math_4_score)
 FROM naep
 GROUP BY state
 ORDER BY state;
 
 --4. 
-WITH scores AS
-(
-	SELECT state, MAX(avg_math_4_score)-MIN(avg_math_4_score) >30 AS diff
-	FROM naep
-	GROUP BY state)
-SELECT state, diff
-FROM scores
-WHERE diff IS TRUE
-GROUP BY state, scores.diff;
+SELECT state, ROUND(AVG(avg_math_4_score),2), MIN(avg_math_4_score),
+	MAX(avg_math_4_score), COUNT(avg_math_4_score)
+FROM naep
+GROUP BY state
+HAVING MAX(avg_math_4_score)-MIN(avg_math_4_score) >30;
 
 --5. Bottom 10 in year 2000
 SELECT state, avg_math_4_score
@@ -32,33 +34,30 @@ ORDER BY avg_math_4_score ASC
 LIMIT 10;
 
 --6. List averages per state in 2000
-SELECT ROUND(AVG(avg_math_4_score),2), state
+SELECT ROUND(SUM(avg_math_4_score)/COUNT(avg_math_4_score),2)
 FROM naep
-WHERE year = 2000
-GROUP BY state
-ORDER BY state;
+WHERE year = 2000;
+--224.80 average across all states
 
 --7. Show only the underperforming states
-SELECT state, ROUND(AVG(avg_math_4_score),2)>SUM(avg_math_4_score)/COUNT(avg_math_4_score) AS below_average_states_y2000
+SELECT state, ROUND(AVG(avg_math_4_score),2) AS below_average_states_y2000
 FROM naep
 WHERE year = 2000
 GROUP BY state
-HAVING ROUND(AVG(avg_math_4_score),2)>SUM(avg_math_4_score)/COUNT(avg_math_4_score)
+HAVING AVG(avg_math_4_score) > 224.80
 ORDER BY state;
 
 --8. Find all states with NULL values
-SELECT state, AVG(avg_math_4_score) IS NULL as scores_missing_y2000
+SELECT state, avg_math_4_score as scores_missing_y2000
 FROM naep
-WHERE year = 2000
-GROUP BY state
-HAVING AVG(avg_math_4_score) IS NULL
+WHERE year = 2000 AND avg_math_4_score IS NULL
+GROUP BY state, avg_math_4_score
 ORDER BY state;
 
 --9.
-SELECT naep.state, ROUND(AVG(avg_math_4_score),2), finance.total_expenditure
+SELECT naep.state, avg_math_4_score, finance.total_expenditure
 FROM naep LEFT OUTER JOIN finance
 ON naep.id = finance.id
-WHERE naep.year = 2000
-GROUP BY naep.state, finance.total_expenditure
-HAVING ROUND(AVG(avg_math_4_score),2) IS NOT NULL
-ORDER BY 2 DESC;
+WHERE naep.year = 2000 AND avg_math_4_score IS NOT NULL
+GROUP BY naep.state, finance.total_expenditure, naep.avg_math_4_score
+ORDER BY 3 DESC;
